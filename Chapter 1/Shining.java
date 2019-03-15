11a4fb995e9ddf74f0800ed111f4476e6e5c6712
Kelly Pratama <46524183+kellytama@users.noreply.github.com>
2019-03-15 12:50:52 -0700

    Create Shining.java

---
+++ Chapter 1/Shining.java
@@ -0,0 +1,57 @@
+/*	
+ * 	This solution is accepted in the practice-it webpage but the question states that you should only use material learned in Ch.1
+ *  (which doesn't include use of for loops), if you try to turn this in as an assignment you may lose points. 
+ */
+
+public class Shining{
+    public static void main(String[] args){
+        for (int i = 0; i < 1000; i++){
+            shine();
+        }
+    }
+    public static void shine(){
+        System.out.println("All work and no play makes Jack a dull boy.");
+    }
+}
+
+// to uncomment this solution when copy-pasting into an ide such as Eclipse, press 'CTRL' + '\'  
+//public class Shining {
+//    public static void main(String[] args) {
+//        printFiveHundred();
+//        printFiveHundred();
+//    }
+//    
+//    public static void printFiveHundred() {
+//        printOneHundred();
+//        printOneHundred();
+//        printOneHundred();
+//        printOneHundred();
+//        printOneHundred();
+//    }
+//    
+//    public static void printOneHundred() {
+//        printFifty();
+//        printFifty();
+//    }
+//    
+//    public static void printFifty() {
+//        printTen();
+//        printTen();
+//        printTen();
+//        printTen();
+//        printTen();
+//    }
+//    
+//    public static void printTen() {
+//        printFive();
+//        printFive();
+//    }
+//    
+//    public static void printFive() {
+//        System.out.println("All work and no play makes Jack a dull boy.");
+//        System.out.println("All work and no play makes Jack a dull boy.");
+//        System.out.println("All work and no play makes Jack a dull boy.");
+//        System.out.println("All work and no play makes Jack a dull boy.");
+//        System.out.println("All work and no play makes Jack a dull boy.");
+//    }
+//}
