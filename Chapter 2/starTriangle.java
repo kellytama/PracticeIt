c68ed0f1fdf8f7d75d1a9e992d69280e734101f2
Kelly Pratama <46524183+kellytama@users.noreply.github.com>
2019-03-15 13:24:08 -0700

    Create starTriangle.java

---
+++ Chapter 2/starTriangle.java
@@ -0,0 +1,14 @@
+for(int i = 1; i <= 5; i++){
+    // outer for loop iterates 5x
+    for(int j = 0; j < i; j++)
+        // prints a star for every increment of i
+        System.out.print("*");
+    System.out.println();
+}
+
+//output
+//*
+//**
+//***
+//****
+//*****
