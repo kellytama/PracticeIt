f716e8e76e59f2390df54f2f2b622a32ed3252d0
Kelly Pratama <46524183+kellytama@users.noreply.github.com>
2019-03-15 12:53:20 -0700

    Create displacement.java

---
+++ Chapter 2/displacement.java
@@ -0,0 +1,20 @@
+//In physics, a common useful equation for finding the position s of a body in linear motion at a given time t, based on its initial //position s0, initial velocity v0, and rate of acceleration a, is the following:
+
+//s = s0 + v0 t + ½ at2
+
+//Write code to declare variables for s0 with a value of 12.0, v0 with a value of 3.5, a with a value of 9.8, and t with a value of 10, //and then write the code to compute s on the basis of these values. At the end of your code, print the value of your variable s to the //console.
+
+double s;
+double initialS;
+double initialV;
+double a;
+double time;
+
+initialS = 12.0;
+initialV = 3.5;
+time = 10;
+a = 9.8;
+
+s = initialS + initialV * time + (0.5 * a * time * time);
+
+System.out.println(s);
