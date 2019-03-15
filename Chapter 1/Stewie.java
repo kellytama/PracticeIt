7377e18677e9c410361d7df064de9513dbe05b24
Kelly Pratama <46524183+kellytama@users.noreply.github.com>
2019-03-15 11:16:46 -0700

    Create Stewie.java
    Write a complete Java program in a class named Stewie that prints the following output:
    //////////////////////
    || Victory is mine! ||
    \\\\\\\\\\\\\\\\\\\\\\
    
    Demonstrates "\\" backslash escape sequence

---
+++ Chapter 1/Stewie.java
@@ -0,0 +1,8 @@
+public class Stewie{
+    public static void main(String[]args){
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");     
+    }
+
+}
