014b64c25b4e1a1184618060cf19d03c595e2414
Kelly Pratama <46524183+kellytama@users.noreply.github.com>
2019-03-15 12:42:22 -0700

    Create Mantra.java

---
+++ Chapter 1/Mantra.java
@@ -0,0 +1,13 @@
+public class Mantra{
+    public static void main(String[] args) {
+        mantra();
+        System.out.println();
+        mantra();
+    }
+    public static void mantra(){
+        System.out.println("There's one thing every coder must understand:\n" +
+                           "The System.out.println command.");
+    }
+    
+    
+}
