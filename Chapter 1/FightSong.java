f8af3e7da2ca105be118c5960ddb556420650c5a
Kelly Pratama <46524183+kellytama@users.noreply.github.com>
2019-03-15 12:44:25 -0700

    Create FightSong.java

---
+++ Chapter 1/FightSong.java
@@ -0,0 +1,26 @@
+public class FightSong {
+    public static void main(String[] args) {
+        goTeam();
+        System.out.println();
+        goTeam();
+        bestWest();
+        System.out.println();
+        goTeam();
+        bestWest();
+        System.out.println();
+        goTeam();
+    }
+                                                    
+    public static void goTeam() {
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+    }
+
+    public static void bestWest() {
+        System.out.println("You're the best,");
+        System.out.println("In the West.");
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+   
+    }
+}
